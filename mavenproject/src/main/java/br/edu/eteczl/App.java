d951f2b48ec4f926e69467cfd8148f44d53aa9b1
guix00 <gui.business007@gmail.com>
2019-02-26 04:36:40 -0300

    Novo projeto Maven

--- mavenproject/src/main/java/br/edu/eteczl/App.java
+++ mavenproject/src/main/java/br/edu/eteczl/App.java
@@ -1,13 +1,33 @@
 package br.edu.eteczl;
-
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends JFrame 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new App();
+    }
+    
+    public App() {
+        setSize (500,500);
+        setVisible(true);
+    }
+    
+    public void paint (Graphics g) {
+        g.drawRect(50,50,100,25);
+        
+        g.setColor(Color.red);
+        g.drawOval(200,50,50,50);
+        
+        g.setColor(Color.green);
+        g.fillRect(50, 200, 100, 25);
+        
+        g.setColor(Color.black);
+        g.drawString("My String broooooooooo", 200,200);
     }
 }
